14a6f16d1a8b2c2fbdf7e0502678a7f3d399a81e
ryandipp <131366236+ryandipp@users.noreply.github.com>
2023-04-26 21:10:17 +0700

    Update process_time_cancel_rate_trend2022.sql

--- scripts/process_time_cancel_rate_trend2022.sql
+++ scripts/process_time_cancel_rate_trend2022.sql
@@ -11,4 +11,15 @@ where date(o.created_at) >= '2022-01-01' and date(o.created_at) < '2023-01-01'
 group by yearly, monthly
 order by yearly asc, monthly asc;
 
+-- CORR PACKING TIME - CANCELLATION RATE ALL DAYS FROM 2019
+select year(o.created_at) as yearly, month(o.created_at) as monthly, day(o.created_at) as daily,
+	avg(timestampdiff(hour, o.created_at, o.shipped_at)) as packing_time,
+    count(case when o.status = 'Cancelled' then o.status else NULL end)/count(o.created_at) as cancel_rate 
+    
+from orders as o
+where date(o.created_at) >= '2019-01-01' and date(o.created_at) < '2023-04-01'
+group by yearly, monthly, daily
+having packing_time is not null and cancel_rate > 0 and cancel_rate <0.5
+order by yearly asc, monthly asc;
+
 	
